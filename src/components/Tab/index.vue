49aac5f82db331fa43902a3b1061d7ba7ed747e9
ASxxxu <892286739@qq.com>
2020-12-22 23:03:18 +0800

    tab

---
+++ src/components/Tab/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane :label="v.name" :name="v.name" v-for="(v,i) in tabList" :key="i"></el-tab-pane>
+    </el-tabs>
+</template>
+
+<script>
+export default {
+    name:"Tab",
+    props:{
+        tabList:{
+            type:Array,
+            default:()=>[]
+        }
+    },
+    methods:{
+        handleClick(v){
+            this.$router.push({path:v.url})
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
